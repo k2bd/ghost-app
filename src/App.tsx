7c157599ddde91a60609acf9deae764e4f1e2314
Kevin Duff <kevinkelduff@gmail.com>
2021-06-08 12:21:47 +0100

    Switch to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import JoinGamePage from './pages/JoinGamePage';
 import GamePage from './pages/GamePage';
@@ -10,14 +10,14 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <HashRouter basename="/">
                 <Switch>
                     <Route path="/ghost" exact component={JoinGamePage} />
                     <Route path="/ghost/:roomCode" component={GamePage} />
 
                     <Redirect to="/ghost" />
                 </Switch>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
