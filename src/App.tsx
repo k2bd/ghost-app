74633af190f48ef48ca7752004c30af90d9d1a83
Kevin Duff <kevinkelduff@gmail.com>
2021-06-08 12:33:07 +0100

    Tidy

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import JoinGamePage from './pages/JoinGamePage';
 import GamePage from './pages/GamePage';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
     return (
         <div className="App">
             <HashRouter basename="/">
                 <Switch>
-                    <Route path="/ghost-app" exact component={JoinGamePage} />
-                    <Route path="/ghost-app/:roomCode" component={GamePage} />
+                    <Route path="/" exact component={JoinGamePage} />
+                    <Route path="/:roomCode" component={GamePage} />
 
-                    <Redirect to="/ghost" />
+                    <Redirect to="/" />
                 </Switch>
             </HashRouter>
         </div>
     );
-}
+};
 
 export default App;
